6ab81dcaf1da389c99e62a0398bb320340a922d9
Hyunju Lee <guswnee00@gmail.com>
2024-04-16 21:45:04 +0900

    Initial commit

---
+++ src/test/java/org/zerobase/restaurantreservationproject/RestaurantReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerobase.restaurantreservationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
