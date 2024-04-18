9f503aecca66c4937a825984f384eb671e3024bc
Hyunju Lee <guswnee00@gmail.com>
2024-04-18 21:36:39 +0900

    user repo init

---
+++ src/main/java/org/zerobase/restaurantreservationproject/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.zerobase.restaurantreservationproject.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.zerobase.restaurantreservationproject.domain.user.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+}
