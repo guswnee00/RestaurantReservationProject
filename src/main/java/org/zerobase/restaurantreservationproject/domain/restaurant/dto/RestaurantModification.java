fea7d2ee6b567b5caf3087f123710a49d9fe6977
Hyunju Lee <guswnee00@gmail.com>
2024-04-21 00:49:26 +0900

    request/response for edit restaurant

---
+++ src/main/java/org/zerobase/restaurantreservationproject/domain/restaurant/dto/RestaurantModification.java
@@ -0,0 +1,46 @@
+package org.zerobase.restaurantreservationproject.domain.restaurant.dto;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+public class RestaurantModification {
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Request {
+
+        private String restaurantName;
+        private String restaurantAddress;
+        private String restaurantDetail;
+
+        private double lat;
+        private double lnt;
+
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response {
+        private String managerId;
+
+        private String restaurantName;
+        private String restaurantAddress;
+        private String restaurantDetail;
+
+        private LocalDateTime modifiedAt;
+
+        public static Response fromDto(RestaurantDto dto) {
+            return Response.builder()
+                    .managerId(dto.getManagerId())
+                    .restaurantName(dto.getRestaurantName())
+                    .restaurantAddress(dto.getRestaurantAddress())
+                    .restaurantDetail(dto.getRestaurantDetail())
+                    .modifiedAt(dto.getModifiedAt())
+                    .build();
+        }
+    }
+}
