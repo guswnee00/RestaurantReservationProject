1138ffcf74be25acfa9fc52fff05351261651f4e
Hyunju Lee <guswnee00@gmail.com>
2024-04-25 00:05:12 +0900

    security config

--- src/main/java/org/zerobase/restaurantreservationproject/global/auth/security/SecurityConfiguration.java
+++ src/main/java/org/zerobase/restaurantreservationproject/global/auth/security/SecurityConfiguration.java
@@ -1,12 +1,52 @@
 package org.zerobase.restaurantreservationproject.global.auth.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.zerobase.restaurantreservationproject.global.role.PersonRole;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // csrf disable
+        http.csrf((auth) -> auth.disable());
+        // form 로그인 방식 disable
+        http.formLogin((auth) -> auth.disable());
+        // http basic 인증 방식 disable
+        http.httpBasic((auth) -> auth.disable());
+        // 경로별 인가 작업
+        http.authorizeHttpRequests((auth) -> auth
+                // TODO
+                //  - 회원가입/로그인 페이지는 누구나 접근 가능
+                .requestMatchers("/").permitAll()
+                // manager 페이지는 점장님만 접근 가능
+                .requestMatchers("/manager/**").hasAuthority(PersonRole.MANAGER.toString()));
+        // TODO
+        //  - 로그인 필터 전 동작
 
+        // 세션 stateless 설정
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
