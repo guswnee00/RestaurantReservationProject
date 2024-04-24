0665997f3104f58c100555dbbd784f3ae6077147
Hyunju Lee <guswnee00@gmail.com>
2024-04-24 21:24:08 +0900

    security config init

---
+++ src/main/java/org/zerobase/restaurantreservationproject/global/auth/security/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package org.zerobase.restaurantreservationproject.global.auth.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+}
