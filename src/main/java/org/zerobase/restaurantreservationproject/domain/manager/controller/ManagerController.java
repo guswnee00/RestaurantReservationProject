0c982900c08b57ac25652335a67871530106bfb4
Hyunju Lee <guswnee00@gmail.com>
2024-04-24 21:23:52 +0900

    edit

--- src/main/java/org/zerobase/restaurantreservationproject/domain/manager/controller/ManagerController.java
+++ src/main/java/org/zerobase/restaurantreservationproject/domain/manager/controller/ManagerController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/manager")
 @RequiredArgsConstructor
-
 public class ManagerController {
 
     @PostMapping("/signup")
