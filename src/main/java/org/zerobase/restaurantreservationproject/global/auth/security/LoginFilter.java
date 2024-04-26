087c4a4bb0068ba757d2fe9c4fad9ec9fc2644fb
Hyunju Lee <guswnee00@gmail.com>
2024-04-27 01:17:02 +0900

    init login filter

---
+++ src/main/java/org/zerobase/restaurantreservationproject/global/auth/security/LoginFilter.java
@@ -0,0 +1,7 @@
+package org.zerobase.restaurantreservationproject.global.auth.security;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+}
