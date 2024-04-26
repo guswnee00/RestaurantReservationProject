476cb2b7a4dbec70dcb5a60922c0f3b81da08ae4
Hyunju Lee <guswnee00@gmail.com>
2024-04-27 01:37:42 +0900

    login filter

--- src/main/java/org/zerobase/restaurantreservationproject/global/auth/security/LoginFilter.java
+++ src/main/java/org/zerobase/restaurantreservationproject/global/auth/security/LoginFilter.java
@@ -1,7 +1,54 @@
 package org.zerobase.restaurantreservationproject.global.auth.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    /**
+     * 로그인 Authentication 을 위한 함수
+     *      1. 사용자로부터 받은 request 에서 username 과 password 추출
+     *      2. username 과 password 를 검증하기 위해 token 에 담기
+     *      3. 검증을 위해 AuthenticationManager 로 전달
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    /**
+     * TODO
+     *  - 로그인 성공시 실행할 메소드
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    /**
+     * TODO
+     *  - 로그인 실패시 실행할 메소드
+     */
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
 }
