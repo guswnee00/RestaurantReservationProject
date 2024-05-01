597a9acfa99362d31445b50e4f9a9fed5371bbd4
Hyunju Lee <guswnee00@gmail.com>
2024-05-01 16:46:52 +0900

    custom user detail

---
+++ src/main/java/org/zerobase/restaurantreservationproject/domain/user/dto/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package org.zerobase.restaurantreservationproject.domain.user.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.zerobase.restaurantreservationproject.domain.user.entity.UserEntity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity userEntity;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return userEntity.getRole();
+            }
+        });
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/org/zerobase/restaurantreservationproject/domain/user/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.zerobase.restaurantreservationproject.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.zerobase.restaurantreservationproject.domain.user.dto.CustomUserDetails;
+import org.zerobase.restaurantreservationproject.domain.user.entity.UserEntity;
+import org.zerobase.restaurantreservationproject.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // DB 조회
+        UserEntity userEntity = userRepository.findByUsername(username);
+
+        // UserDetail 에 담아서 return 하면 AuthenticationManager 가 검증
+        if (userEntity != null) {
+            return new CustomUserDetails(userEntity);
+        }
+        return null;
+    }
+}
