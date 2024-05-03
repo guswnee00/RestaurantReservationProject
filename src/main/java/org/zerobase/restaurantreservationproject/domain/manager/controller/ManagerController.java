cc2c8c994bcd8555e78192c3d91dfc69cf5bdf96
Hyunju Lee <guswnee00@gmail.com>
2024-05-04 03:20:12 +0900

    add/modify restaurant

--- src/main/java/org/zerobase/restaurantreservationproject/domain/manager/controller/ManagerController.java
+++ src/main/java/org/zerobase/restaurantreservationproject/domain/manager/controller/ManagerController.java
@@ -2,13 +2,19 @@ package org.zerobase.restaurantreservationproject.domain.manager.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.zerobase.restaurantreservationproject.domain.manager.dto.Manager;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.zerobase.restaurantreservationproject.domain.manager.dto.ManagerAddition;
 import org.zerobase.restaurantreservationproject.domain.manager.dto.ManagerDto;
+import org.zerobase.restaurantreservationproject.domain.manager.entity.ManagerEntity;
 import org.zerobase.restaurantreservationproject.domain.manager.service.ManagerService;
+import org.zerobase.restaurantreservationproject.domain.restaurant.dto.RestaurantAddition;
+import org.zerobase.restaurantreservationproject.domain.restaurant.dto.RestaurantDto;
+import org.zerobase.restaurantreservationproject.domain.restaurant.dto.RestaurantModification;
+import org.zerobase.restaurantreservationproject.global.error.CustomException;
+
+import static org.zerobase.restaurantreservationproject.global.error.CustomErrorCode.MANAGER_DOES_NOT_SAME;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,28 +24,44 @@ public class ManagerController {
     private final ManagerService managerService;
 
     /**
-     * 매장 점장님 회원가입
+     * 매니저 회원가입
      */
     @PostMapping("/manager/signup")
-    public ResponseEntity<?> managerSignup(@RequestBody Manager.Request request) {
+    public ResponseEntity<?> managerSignup(@RequestBody ManagerAddition.Request request) {
         ManagerDto managerDto = managerService.signup(request);
-        return ResponseEntity.ok(Manager.Response.fromDto(managerDto));
+        return ResponseEntity.ok(ManagerAddition.Response.fromDto(managerDto));
     }
 
-    /*
-
+    /**
+     * 매니저의 매장 등록
+     */
     @PreAuthorize("hasRole('ROLE_MANAGER')")
-    @PostMapping("/manager/restaurant-registration")
-    public ResponseEntity<?> registerRestaurant() {
-        return ResponseEntity.ok();
+    @PostMapping("/manager/restaurant-registration/{username}")
+    public ResponseEntity<?> registerRestaurant(@PathVariable String username,
+                                                @RequestBody RestaurantAddition.Request request,
+                                                @AuthenticationPrincipal ManagerEntity manager
+    ) {
+        if(!username.equals(manager.getUsername())) {
+            throw new CustomException(MANAGER_DOES_NOT_SAME);
+        }
+        RestaurantDto restaurantDto = managerService.addRestaurant(request);
+        return ResponseEntity.ok(RestaurantAddition.Response.fromDto(restaurantDto));
     }
 
+    /**
+     * 매니저의 매장 수정
+     */
     @PreAuthorize("hasRole('ROLE_MANAGER')")
-    @PutMapping("/manager/restaurant-modification/{managerId}")
-    public ResponseEntity<?> modifyRestaurant() {
-        return ResponseEntity.ok();
+    @PutMapping("/manager/restaurant-modification/{username}")
+    public ResponseEntity<?> modifyRestaurant(@PathVariable String username,
+                                              @RequestBody RestaurantModification.Request request,
+                                              @AuthenticationPrincipal ManagerEntity manager
+    ) {
+        if(!username.equals(manager.getUsername())) {
+            throw new CustomException(MANAGER_DOES_NOT_SAME);
+        }
+        RestaurantDto restaurantDto = managerService.modifyRestaurant(request);
+        return ResponseEntity.ok(RestaurantModification.Response.fromDto(restaurantDto));
     }
-
-    */
 
 }
--- src/main/java/org/zerobase/restaurantreservationproject/domain/manager/service/ManagerService.java
+++ src/main/java/org/zerobase/restaurantreservationproject/domain/manager/service/ManagerService.java
@@ -4,13 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.zerobase.restaurantreservationproject.domain.manager.dto.Manager;
+import org.zerobase.restaurantreservationproject.domain.manager.dto.ManagerAddition;
 import org.zerobase.restaurantreservationproject.domain.manager.dto.ManagerDto;
 import org.zerobase.restaurantreservationproject.domain.manager.entity.ManagerEntity;
 import org.zerobase.restaurantreservationproject.domain.manager.repository.ManagerRepository;
+import org.zerobase.restaurantreservationproject.domain.restaurant.dto.RestaurantAddition;
+import org.zerobase.restaurantreservationproject.domain.restaurant.dto.RestaurantDto;
+import org.zerobase.restaurantreservationproject.domain.restaurant.dto.RestaurantModification;
+import org.zerobase.restaurantreservationproject.domain.restaurant.entity.RestaurantEntity;
+import org.zerobase.restaurantreservationproject.domain.restaurant.repository.RestaurantRepository;
 import org.zerobase.restaurantreservationproject.global.error.CustomException;
 
-import static org.zerobase.restaurantreservationproject.global.error.CustomErrorCode.ID_IS_ALREADY_IN_USE;
+import static org.zerobase.restaurantreservationproject.global.error.CustomErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -18,9 +23,18 @@ import static org.zerobase.restaurantreservationproject.global.error.CustomError
 public class ManagerService {
 
     private final ManagerRepository managerRepository;
+    private final RestaurantRepository restaurantRepository;
+
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public ManagerDto signup(Manager.Request request) {
+    /**
+     * 매니저 회원가입
+     *      1. 같은 이름 있는지 확인
+     *      2. 비밀번호 encode
+     *      3. 매니저 정보 repo 에 저장
+     *      4. 매니저 dto 반환
+     */
+    public ManagerDto signup(ManagerAddition.Request request) {
 
         // 같은 유저이름(아이디)가 있다면 예외 발생
         if (managerRepository.existsByUsername(request.getUsername())) {
@@ -30,10 +44,71 @@ public class ManagerService {
         // 비밀번호 encode
         request.setPassword(bCryptPasswordEncoder.encode(request.getPassword()));
 
-        // 사용자 정보 저장
-        ManagerEntity managerEntity = managerRepository.save(Manager.Request.toEntity(request));
+        // 매니저 정보 저장
+        ManagerEntity managerEntity = managerRepository.save(ManagerAddition.Request.toEntity(request));
 
         return ManagerDto.toDto(managerEntity);
     }
+
+    /**
+     * 매니저의 레스토랑 등록
+     *      1. 매니저 request 에서 매니저의 아이디를 추출
+     *      2. 매니저가 존재하는지, 매니저가 이미 레스토랑을 등록했는지 확인후
+     *      3. 레스토랑 정보를 repo 에 등록
+     *      4. 레스토랑 dto 반환
+     */
+    public RestaurantDto addRestaurant(RestaurantAddition.Request request) {
+
+        // 매니저가 있는지 확인
+        this.checkManagerExist(request.getManagerName());
+        // 매니저가 이미 레스토랑을 등록했는지 확인
+        this.checkRestaurantExist(request.getManagerName());
+
+        RestaurantEntity restaurantEntity = restaurantRepository.save(RestaurantAddition.Request.toEntity(request, managerRepository));
+
+        return RestaurantDto.toDto(restaurantEntity);
+
+    }
+
+    /**
+     * 매니저의 레스토랑 정보 수정
+     *      1. 레스토랑 정보가 있는지 확인
+     *      2. 레스토랑 정보 수정
+     *      3. 수정된 정보 repo 에 저장
+     *      4. 레스토랑 dto 반환
+     */
+    public RestaurantDto modifyRestaurant(RestaurantModification.Request request) {
+
+        // 레스토랑 정보가 있는지 확인
+        RestaurantEntity oldRestaurantEntity = restaurantRepository.findByManager_Username(request.getManagerName());
+        this.checkRestaurantNull(oldRestaurantEntity);
+
+        // 수정
+        oldRestaurantEntity.modify(request);
+
+        RestaurantEntity newRestaurantEntity = restaurantRepository.save(oldRestaurantEntity);
+
+        return RestaurantDto.toDto(newRestaurantEntity);
+
+    }
+
+    private void checkManagerExist(String managerName) {
+        if(!managerRepository.existsByUsername(managerName)) {
+            throw new CustomException(MANAGER_DOES_NOT_EXIST);
+        }
+    }
+
+    private void checkRestaurantExist(String username) {
+        if (restaurantRepository.existsByManager_Username(username)) {
+            throw new CustomException(ALREADY_REGISTERED_BY_MANAGER);
+        }
+    }
+
+    private void checkRestaurantNull(RestaurantEntity entity) {
+        if(entity == null) {
+            throw new CustomException(RESTAURANT_INFO_DOES_NOT_EXIST);
+        }
+    }
+
 
 }
