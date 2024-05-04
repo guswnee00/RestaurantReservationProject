391289b6c94f03d0131766feac8ebaeafbe0102d
Hyunju Lee <guswnee00@gmail.com>
2024-05-04 23:26:21 +0900

    sorting enum

--- src/main/java/org/zerobase/restaurantreservationproject/global/enumset/SortRestaurant.java
+++ src/main/java/org/zerobase/restaurantreservationproject/global/enumset/SortRestaurant.java
@@ -1,4 +1,10 @@
 package org.zerobase.restaurantreservationproject.global.enumset;
 
 public enum SortRestaurant {
+
+    NEAR_DISTANCE,      // 가까운 거리순
+    HIGH_STAR_RATING,   // 별점순
+    ALPHABETIZATION,    // 가나다순
+    MANY_REVIEWS        // 리뷰 많은 순
+
 }
